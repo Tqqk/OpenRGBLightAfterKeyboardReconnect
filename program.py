e4dca365d7e3d027aa1d0477644e16c5d5dd5e44
Techkkq <93881136+Tqqk@users.noreply.github.com>
2023-10-15 19:51:44 +0200

    v2

--- program.py
+++ program.py
@@ -1,59 +1,51 @@
 import subprocess
 import time
-import psutil
 
-# Set the devcon path to "devcon.exe" in the same folder
+profile_name = "PROFILE_NAME"
+
+# Variables
+openrgb_path = "OpenRGB.exe"  
 devcon_path = "devcon.exe"
-
-# Device classes for keyboards and mice
 keyboard_class = "HIDClass"
 mouse_class = "Mouse"
 
-# Path to OpenRGB executable and profile name in the same folder
-openrgb_path = "OpenRGB.exe"  # Use just the executable name
-profile_name = "color"
-
-# Create startupinfo object
 startupinfo = subprocess.STARTUPINFO()
 startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 startupinfo.wShowWindow = subprocess.SW_HIDE
 
 def count_hid_devices(device_class):
     try:
-        output = subprocess.check_output([devcon_path, 'find', '='+device_class], universal_newlines=True, startupinfo=startupinfo)
-        # Count the number of lines (each line corresponds to a device)
-        device_count = len(output.split('\n'))
+        # Devices counted by devcon
+        process = subprocess.Popen([devcon_path, 'find', '='+device_class], stdout=subprocess.PIPE, universal_newlines=True, startupinfo=startupinfo)
+        device_count = sum(1 for _ in process.stdout) 
+        process.stdout.close()
         return device_count
     except subprocess.CalledProcessError as e:
         print("Error running devcon:", e)
         return 0
 
 def start_openrgb():
-    # Check if OpenRGB is already running
-    for proc in psutil.process_iter(['pid', 'name']):
-        if 'OpenRGB.exe' in proc.info['name']:
-            print('Killing OpenRGB...')
-            proc.kill()
+    # Restart OpenRGB
+    subprocess.run(['taskkill', '/IM', openrgb_path, '/F'], startupinfo=startupinfo)
 
-    # Start OpenRGB
     print('Starting OpenRGB...')
     subprocess.Popen([openrgb_path, '--startminimized', '--profile', profile_name], stderr=subprocess.PIPE, startupinfo=startupinfo)
 
-# Initial state of device counts
+# Device counts
 prev_hid_device_count = count_hid_devices(keyboard_class) + count_hid_devices(mouse_class)
 
 while True:
-    # Check current HID device counts
+    # HID device counts
     current_hid_device_count = count_hid_devices(keyboard_class) + count_hid_devices(mouse_class)
 
-    # Print device count for debugging
+    # Debug
     print("Current HID device count:", current_hid_device_count)
 
-    # Check for HID device count change
+    # HID device count change
     if current_hid_device_count != prev_hid_device_count:
-        # Perform actions when the HID device count changes
+        
         print("HID device count changed. Restarting OpenRGB.")
-        start_openrgb()  # Restart OpenRGB when the HID device count changes
+        start_openrgb()  
 
     prev_hid_device_count = current_hid_device_count
-    time.sleep(1)
+    time.sleep(2)
