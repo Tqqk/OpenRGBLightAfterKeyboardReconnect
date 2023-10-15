db42c057f4c386837150a54fed68382cbbc2bf2c
Techkkq <93881136+Tqqk@users.noreply.github.com>
2023-10-15 18:51:49 +0200

    maybe works

--- program.py
+++ program.py
@@ -1,42 +1,59 @@
 import subprocess
 import time
+import psutil
 
-# Path to the defcon file (you can download it at microsoft's website) - replace the "C:/PATH_TO_DEVCON/devcon.exe" with your own path to the devcon.exe
-devcon_path = r"C:/PATH_TO_DEVCON/devcon.exe"
+# Set the devcon path to "devcon.exe" in the same folder
+devcon_path = "devcon.exe"
 
-# Device ID of your keyboard, can be found in device manager -> KEYBOARDS -> disconnect your keyboard to find out which keyboard dissapears and which doesn't -> click on the keyboard that dissapeared when disconnected -> click on DETAILS -> find HARDWARE IDS in the dropdown menu -> copy the top one (if it doesn't work try the others) - replace "KEYBOARD_ID" with your own keyboard ID
-keyboard_id = "KEYBOARD_ID"
+# Device classes for keyboards and mice
+keyboard_class = "HIDClass"
+mouse_class = "Mouse"
 
-# Initial state of keyboard
-keyboard_connected = True
+# Path to OpenRGB executable and profile name in the same folder
+openrgb_path = "OpenRGB.exe"  # Use just the executable name
+profile_name = "color"
 
 # Create startupinfo object
 startupinfo = subprocess.STARTUPINFO()
 startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 startupinfo.wShowWindow = subprocess.SW_HIDE
 
-while True:
-    devcon_find_cmd = [devcon_path, 'status', keyboard_id]
-
+def count_hid_devices(device_class):
     try:
-        output = subprocess.check_output(devcon_find_cmd, universal_newlines=True, startupinfo=startupinfo)
-        if "No matching devices found" in output:
-            if keyboard_connected:
-                
-                keyboard_connected = False
-        else:
-            if not keyboard_connected:
-                
-                keyboard_connected = True
-                
-                # Kill the OpenRGB task
-                subprocess.run('taskkill /IM OpenRGB.exe /F', startupinfo=startupinfo)
-
-                # Start the OpenRGB program with the desired attributes - replace the "C:\\PATH_TO_OPENRGB\\OpenRGB.exe" with you own OpenRGB.exe path and replace the "RGB_PROFILE_NAME" to the name of your profile in OpenRGB.
-
-                process = subprocess.Popen(['C:\\PATH_TO_OPENRGB\\OpenRGB.exe', '--startminimized', '--profile', 'RGB_PROFILE_NAME'], stderr=subprocess.PIPE, startupinfo=startupinfo)
-
+        output = subprocess.check_output([devcon_path, 'find', '='+device_class], universal_newlines=True, startupinfo=startupinfo)
+        # Count the number of lines (each line corresponds to a device)
+        device_count = len(output.split('\n'))
+        return device_count
     except subprocess.CalledProcessError as e:
         print("Error running devcon:", e)
+        return 0
 
+def start_openrgb():
+    # Check if OpenRGB is already running
+    for proc in psutil.process_iter(['pid', 'name']):
+        if 'OpenRGB.exe' in proc.info['name']:
+            print('Killing OpenRGB...')
+            proc.kill()
+
+    # Start OpenRGB
+    print('Starting OpenRGB...')
+    subprocess.Popen([openrgb_path, '--startminimized', '--profile', profile_name], stderr=subprocess.PIPE, startupinfo=startupinfo)
+
+# Initial state of device counts
+prev_hid_device_count = count_hid_devices(keyboard_class) + count_hid_devices(mouse_class)
+
+while True:
+    # Check current HID device counts
+    current_hid_device_count = count_hid_devices(keyboard_class) + count_hid_devices(mouse_class)
+
+    # Print device count for debugging
+    print("Current HID device count:", current_hid_device_count)
+
+    # Check for HID device count change
+    if current_hid_device_count != prev_hid_device_count:
+        # Perform actions when the HID device count changes
+        print("HID device count changed. Restarting OpenRGB.")
+        start_openrgb()  # Restart OpenRGB when the HID device count changes
+
+    prev_hid_device_count = current_hid_device_count
     time.sleep(1)
